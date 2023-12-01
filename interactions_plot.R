54d9cb87669e7c77271d1b62b9781d108224ea14
statransform <statransform@gmail.com>
2023-12-01 16:44:13 +0100

    Section 3 cont.

--- interactions_plot.R
+++ interactions_plot.R
@@ -65,4 +65,52 @@ createInteractionPlot <- function(data, likert = FALSE, logscale = FALSE) {
   p
 }
 
-df <- read.csv("removable_interactions.csv", sep=",", header=TRUE, strip.white=TRUE)
+
+createRankInteractionPlot <- function(data, rnkscale = FALSE) {
+  symbols <- c("diamond", "x-thin")
+  palette <- c("#888888", "#FF5E00")
+
+  ytitle <- ifelse(rnkscale, 'rank(Y)', 'Y')
+  range <- if(rnkscale) c(0, max(data$Y) + 10) else c(min(data$Y) - 2, max(data$Y) + 2)
+
+  techA <- list(
+  xref = 'paper',
+    yref = 'y',
+    x = 0.55,
+    y = (data[data$Technique == "Technique A",]$Y[1] + data[data$Technique == "Technique A",]$Y[3])/2,
+    xanchor = 'left',
+    yanchor = 'top',
+    text = "Technique A",
+    font = list(family = 'Arial',
+              size = 16,
+              color = palette[1]),
+    showarrow = FALSE
+  )
+
+  techB <- list(
+  xref = 'paper',
+    yref = 'y',
+    x = 0.44,
+    y = (data[data$Technique == "Technique B",]$Y[1] + data[data$Technique == "Technique B",]$Y[3])/2,
+    xanchor = 'right',
+    yanchor = 'bottom',
+    text = "Technique B",
+    font = list(family = 'Arial',
+              size = 16,
+              color = palette[2]),
+    showarrow = FALSE
+  )
+
+  p <- plot_ly(data,  x = ~factor(Difficulty), y = ~Y, color = ~Technique, symbol = ~Technique, linetype = ~Technique, symbols = symbols, 
+    colors = palette, type = 'scatter', mode = 'lines+markers', line = list(width = 4), marker = list(line = list(width = 4)), showlegend = FALSE) %>%
+  layout(
+    xaxis = list(title = 'Difficulty', showline=T, mirror = F, fixedrange=T, ticks="outside",tickangle=0, tickfont = list(size = 12)),
+    yaxis = list(title = ytitle, font = list(size = 13), zeroline = F, showline=T, 
+      linewidth=1, mirror = F,  nticks=4, ticks="inside", tickfont = list(size = 12), range = range)
+      #ticktext = lticktext, tickvals = ltickvals)
+  ) %>% layout(annotations = techA)  %>% layout(annotations = techB)  %>% 
+        config(displayModeBar = TRUE, displaylogo = FALSE, modeBarButtonsToRemove = c("lasso2d", "select2d",  "zoomIn2d", "zoomOut2d", "autoscale")) %>% 
+        layout(margin = list(l = 60, r = 0, b = 0, t = 20, pad = 0))
+
+  p
+}
