d4257957ab89ad948d5b10b65efeaf31ae2cdc6e
statransform <statransform@gmail.com>
2024-04-19 17:29:47 +0200

    recommendations...

--- interactions_plot.R
+++ interactions_plot.R
@@ -1,9 +1,7 @@
 
 
 # Code for plotting removable interaction effects of figure 2 using plotly
-createInteractionPlot <- function(data, likert = FALSE, logscale = FALSE) {
-  symbols <- c("diamond", "x-thin")
-  palette <- c("#888888", "#FF5E00")
+createInteractionPlot <- function(data, likert = FALSE, logscale = FALSE,  palette = c("#888888", "#FF5E00"), symbols = c("diamond", "x-thin"), offx = 0) {
 
   if(likert) {
     colnames(data)[3] <- "Time" # Change the name of the colun to Time to make the code consistent
@@ -24,9 +22,9 @@ createInteractionPlot <- function(data, likert = FALSE, logscale = FALSE) {
   techA <- list(
 	xref = 'paper',
   	yref = 'y',
-  	x = if(logscale) 0.62 else 0.52,
-  	y = if(logscale) log10((data[data$Technique == "Technique A",]$Time[1] + data[data$Technique == "Technique A",]$Time[2])/2)
-  		else (data[data$Technique == "Technique A",]$Time[1] + data[data$Technique == "Technique A",]$Time[2])/2,
+  	x = if(logscale) 0.62 else 0.52 + offx,
+  	y = if(logscale) log10((data[grepl("A", data$Technique),]$Time[1] + data[grepl("A", data$Technique),]$Time[2])/2)
+  		else (data[grepl("A", data$Technique),]$Time[1] + data[grepl("A", data$Technique),]$Time[2])/2,
   	xanchor = 'left',
   	yanchor = 'top',
   	text = "Tech A",
@@ -40,8 +38,8 @@ createInteractionPlot <- function(data, likert = FALSE, logscale = FALSE) {
 	xref = 'paper',
   	yref = 'y',
   	x = if(logscale) 0.62 else 0.52,
-  	y = if(logscale) log10((data[data$Technique == "Technique B",]$Time[1] + data[data$Technique == "Technique B",]$Time[2])/2)
-  		else (data[data$Technique == "Technique B",]$Time[1] + data[data$Technique == "Technique B",]$Time[2])/2,
+  	y = if(logscale) log10((data[grepl("B", data$Technique),]$Time[1] + data[grepl("B", data$Technique),]$Time[2])/2)
+  		else (data[grepl("B", data$Technique),]$Time[1] + data[grepl("B", data$Technique),]$Time[2])/2,
   	xanchor = 'left',
   	yanchor = 'top',
   	text = "Tech B",
@@ -65,10 +63,71 @@ createInteractionPlot <- function(data, likert = FALSE, logscale = FALSE) {
   p
 }
 
+# Code for plotting removable interaction effects of figure 2 using plotly -- Same as above but we inverse the variables
+createInteractionPlotInv <- function(data, likert = FALSE, logscale = FALSE, palette = c("#888888", "#FF5E00"), symbols = c("diamond", "x-thin"), offx = 0, offy = 0) {
 
-createRankInteractionPlot <- function(data, rnkscale = FALSE) {
-  symbols <- c("diamond", "x-thin")
-  palette <- c("#888888", "#FF5E00")
+  if(likert) {
+    colnames(data)[3] <- "Time" # Change the name of the colun to Time to make the code consistent
+    ytitle <- 'Perceived performance'
+    type <- '-'
+    range <- c(0.3, 5.1)
+    lticktext <- list("very quick", "quick", "average", "slow", "very slow")
+    ltickvals <- list(1,2,3,4,5)
+  } else {
+    ytitle <- ifelse(logscale, 'Time (min) - log scale', 'Time (min)')
+    type <- ifelse(logscale, 'log', '-')
+    range <- if(logscale) c(log10(0.25), log10(ceiling(max(data$Time)) + 0.1)) else c(0, ceiling(max(data$Time)) + 0.1)
+    lticktext <- if(logscale) list("0.25", "0.5", "1", "2", "4") else list("0", "1", "2", "3", "4")
+    ltickvals <- if(logscale) list(0.25, 0.5, 1, 2, 4) else list(0, 1, 2, 3, 4)
+  }
+
+
+  Easy <- list(
+  xref = 'paper',
+    yref = 'y',
+    x = if(logscale) 0.62 else 0.52 + offx,
+    y = if(logscale) log10((data[data$Difficulty == "easy",]$Time[1] + data[data$Difficulty == "easy",]$Time[2])/2)
+      else (data[data$Difficulty == "easy",]$Time[1] + data[data$Difficulty == "easy",]$Time[2])/2,
+    xanchor = 'left',
+    yanchor = 'top',
+    text = "easy",
+    font = list(family = 'Arial',
+              size = 14,
+              color = palette[1]),
+    showarrow = FALSE
+  )
+
+  Hard <- list(
+  xref = 'paper',
+    yref = 'y',
+    x = if(logscale) 0.62 else 0.52,
+    y = if(logscale) log10((data[data$Difficulty == "hard",]$Time[1] + data[data$Difficulty == "hard",]$Time[2])/2)
+      else (data[data$Difficulty == "hard",]$Time[1] + data[data$Difficulty == "hard",]$Time[2])/2 + offy,
+    xanchor = 'left',
+    yanchor = 'top',
+    text = "hard",
+    font = list(family = 'Arial',
+              size = 14,
+              color = palette[2]),
+    showarrow = FALSE
+  )
+
+  p <- plot_ly(data,  x = ~factor(Technique), y = ~Time, color = ~Difficulty, symbol = ~Difficulty, linetype = ~Difficulty, symbols = symbols, 
+    colors = palette, type = 'scatter', mode = 'lines+markers', line = list(width = 4), marker = list(line = list(width = 4)), showlegend = FALSE) %>%
+  layout(
+    xaxis = list(title = 'Technique', showline=T, mirror = F, fixedrange=T, ticks="outside",tickangle=0, tickfont = list(size = 12)),
+    yaxis = list(type = type, title = ytitle, font = list(size = 13), zeroline = F, showline=T, 
+      linewidth=1, mirror = F,  nticks=4, ticks="inside", tickfont = list(size = 12), range = range, ticktext = lticktext, tickvals = ltickvals, 
+      tickangle = ifelse(likert, -30, 0))
+  ) %>% layout(annotations = Easy)  %>% layout(annotations = Hard)  %>% 
+        config(displayModeBar = TRUE, displaylogo = FALSE, modeBarButtonsToRemove = c("lasso2d", "select2d",  "zoomIn2d", "zoomOut2d", "autoscale")) %>% 
+        layout(margin = list(l = 40, r = 0, b = 0, t = 40, pad = 0))
+
+  p
+}
+
+
+createRankInteractionPlot <- function(data, rnkscale = FALSE, symbols = c("diamond", "x-thin"), palette = c("#888888", "#FF5E00")) {
 
   ytitle <- ifelse(rnkscale, 'rank(Y)', 'Y')
   range <- if(rnkscale) c(0, max(data$Y) + 10) else c(min(data$Y) - 2, max(data$Y) + 2)
